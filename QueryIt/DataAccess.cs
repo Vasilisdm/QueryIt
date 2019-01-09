062fb23b80c7d6565a5ed3ffc95d548bafb6e33e
vasilis <vasilisdmr@gmail.com>
2019-01-10 01:00:10 +0200

    created the IRepository interface

--- QueryIt/DataAccess.cs
+++ QueryIt/DataAccess.cs
@@ -7,8 +7,19 @@ using System.Threading.Tasks;
 
 namespace QueryIt
 {
+
     public class EmployeeDb : DbContext
     {
         public DbSet<Employee> Employees { get; set; }
+    }
+    
+
+    public interface IRepository<T> : IDisposable
+    {
+        void Add(T newEntity);
+        void Delete(T entity);
+        T FindById(int id);
+        IQueryable<T> FindAll();
+        int Commit();
     }
 }
