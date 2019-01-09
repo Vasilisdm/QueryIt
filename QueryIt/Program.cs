9ac0ee88a46b1febe6593f59469167acd1bcfabe
vasilis <vasilisdmr@gmail.com>
2019-01-10 01:18:03 +0200

    instructed the program to drop and recreate the db when a change occurs

--- QueryIt/Program.cs
+++ QueryIt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace QueryIt
     {
         static void Main(string[] args)
         {
+
+            Database.SetInitializer(new DropCreateDatabaseAlways<EmployeeDb>());
         }
     }
 }
